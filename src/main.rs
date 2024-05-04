0e9587b4afb71ec75d311d76f68d1c09b99e9c00
robbani <shahriyar.robbani@embl.de>
2024-05-04 21:45:55 +0200

    write output to stderr

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     // NOTE: Use `unwrap_or_else` to handle errors in the outermost layer
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     // Use `if let Err(e)` to handle functions that only return errors
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
